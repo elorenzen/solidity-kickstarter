91c064287f91e75ae7db0a068240fc383cf51f23
elorenzen <eric.lorenzen@gmail.com>
2019-08-15 14:45:32 -0600

    Add .next bundle and allow window load even without metamask installed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import factory from '../ethereum/factory';
 
-export default () => {
-    return (
-        <h1>This is the INDEX page!</h1>
-    )
+export default class CampaignIndex extends React.Component {
+    async componentDidMount() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        console.log(campaigns);
+    }
+    render() {
+        return (
+            <h1>Show page!!!!!</h1>
+        )
+    }
 };
