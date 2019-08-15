442bae249e9f7dcd8ba89c4c1bbfb36ef3617da3
elorenzen <eric.lorenzen@gmail.com>
2019-08-15 08:26:39 -0600

    Revert Commitde web3 and contract instance" Revert Commit This reverts commit 0416b6ea88bac53a4a67fde43797b343fd6a15cd.
    evert commit

--- ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignDeployment.json';
-
-const contractInstance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0xfC4d4acac37a8747272d2d7f02C7F7dEA6B0Ec89'
-);
-
-export default contractInstance;
