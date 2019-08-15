52c1388c964a1b9f815e52f00cc59c83095ca0d4
elorenzen <eric.lorenzen@gmail.com>
2019-08-15 08:13:07 -0600

    Install next.js and setup root route and show page

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/ericlorenzen/Desktop/Udemy/SG-Solidity/kickstart/pages/index.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, 'This is the INDEX page!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFESixBQUNJLEFBRVA7QUFKRCIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvZXJpY2xvcmVuemVuL0Rlc2t0b3AvVWRlbXkvU0ctU29saWRpdHkva2lja3N0YXJ0In0=
