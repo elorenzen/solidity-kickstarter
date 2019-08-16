21c506afefe8e88b79e151dcf5489f56202f7d47
elorenzen <eric.lorenzen@gmail.com>
2019-08-16 11:52:46 -0600

    Pass contract data to show page

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 export default class CampaignShow extends React.Component {
+    static async getInitialProps(props) {
+        
+        const campaign = Campaign(props.query.campaign_id);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        console.log(summary);
+        
+        return {};
+    };
+
     render() {
         return (
             <Layout>
