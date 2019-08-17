e7a525aec23d0c254c53e774d00481ad3bc697e3
elorenzen <eric.lorenzen@gmail.com>
2019-08-16 19:02:49 -0600

    add cards in show with coontract data

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import { Card } from 'semantic-ui-react';
 
 export default class CampaignShow extends React.Component {
     static async getInitialProps(props) {
@@ -9,15 +10,51 @@ export default class CampaignShow extends React.Component {
 
         const summary = await campaign.methods.getSummary().call();
 
-        console.log(summary);
+        console.log(summary['0']);
         
-        return {};
+        return { 
+            minimumContribution: summary['0'],
+            balance: summary['1'],
+            totalRequests: summary['2'],
+            approversCount: summary['3'],
+            creator: summary['4']
+        };
     };
+
+    renderCards() {
+        const {
+            minimumContribution,
+            balance,
+            totalRequests,
+            approversCount,
+            creator
+        } = this.props;
+
+        const items = [
+            {
+                header: creator,
+                meta: 'Creator Address',
+                description: 'The creator of this campaign can make requests to withdraw money.',
+                style: { overflowWrap: 'break-word' }
+            },
+            {
+                header: minimumContribution,
+                meta: 'Minimum Contribution',
+                description: 'This is the minimum contribution required to join this campaign.'
+            },
+            {
+                header: totalRequests,
+                meta: 'Total Requests',
+                description: 'This is the number of requests.'
+            }
+        ]
+        return <Card.Group items={items} />
+    }
 
     render() {
         return (
             <Layout>
-                <div>Campaign NAME_GOES_HERE</div>
+                {this.renderCards()}
             </Layout>
         )
     }
