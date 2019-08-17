c0428c49babe398a8fed86b2788c808ea54091e9
elorenzen <eric.lorenzen@gmail.com>
2019-08-17 11:57:34 -0600

    Add button on show page to route to newly created requests list page

--- routes.js
+++ routes.js
@@ -2,5 +2,6 @@ const routes = require('next-routes')();
 
 routes.add('/campaigns/new', '/campaigns/new');
 routes.add('/campaigns/:campaign_id', '/campaigns/show');
+routes.add('/campaigns/:campaign_id/requests', '/campaigns/requests/index');
 
 module.exports = routes;
